c5a0059f5a9e1fcd329ef8dba7e656e6496a2b7c
scillidan <scillidan@yandex.com>
2026-01-19 22:01:04 +0800

    upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# disco
+
+[![CC0 1.0][cc-zero-image]][cc-zero]
+
+[cc-zero]: http://creativecommons.org/licenses/zero/1.0/
+[cc-zero-image]: https://i.creativecommons.org/l/zero/1.0/88x31.png
+[cc-zero-shield]: https://img.shields.io/badge/License-CC%20ZERO%201.0-lightgrey.svg
---
+++ lg-video.js
@@ -0,0 +1,28 @@
+jQuery("#lg-video")
+  .justifiedGallery({
+    rowHeight: 180,
+    margins: 1.5,
+    lastRow: 'nojustify',
+    captions: true,
+  })
+  .on("jg.complete", function () {
+    window.lightGallery(
+      document.getElementById("lg-video"),
+      {
+        galleryId: "video",
+        plugins: [lgThumbnail, lgVideo],
+        autoplayFirstVideo: false,
+        addClass: 'lg-custom-thumbnails',
+        download: false,
+        allowMediaOverlap: false,
+        // animateThumb: false,
+        // closeOnTap: false,
+        // enableDrag: false,
+        // enableSwipe: false,
+        pager: false,
+        mousewheel: true,
+        swipeToClose: false,
+        videojs: true,
+      }
+    );
+  });
